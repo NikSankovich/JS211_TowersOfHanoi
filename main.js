d1c32a9a0a3c53a07c32a1febf7fdec1c66aa7fd
NikSankovich <nik.sankovich@gmail.com>
2023-02-18 22:53:41 -0600

    first commit

--- main.js
+++ main.js
@@ -2,19 +2,20 @@
 
 const assert = require('assert');
 const readline = require('readline');
+const { start } = require('repl');
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout
 });
 
 // An object that represents the three stacks of Towers of Hanoi; 
-  // * each key is an array of Numbers: 
-    // * A is the far-left, 
-    // * B is the middle, 
-    // * C is the far-right stack
-      // * Each number represents the largest to smallest tokens: 
-        // * 4 is the largest, 
-        // * 1 is the smallest
+// * each key is an array of Numbers: 
+// * A is the far-left, 
+// * B is the middle, 
+// * C is the far-right stack
+// * Each number represents the largest to smallest tokens: 
+// * 4 is the largest, 
+// * 1 is the smallest
 
 let stacks = {
   a: [4, 3, 2, 1],
@@ -30,26 +31,47 @@ const printStacks = () => {
 }
 
 // Next, what do you think this function should do?
-const movePiece = () => {
-  // Your code here
+const movePiece = (startStack, endStack) => {
+  // pop() the last piece in the startStack and push() to the end of the endStack
+  let piece = stacks[startStack].pop()    // maybe stacks[startStacks]
+
+  stacks[endStack].push(piece)
 
 }
 
 // Before you move, should you check if the move it actually allowed? Should 3 be able to be stacked on 2
-const isLegal = () => {
-  // Your code here
+const isLegal = (startStack, endStack) => {
+  if (stacks[endStack].length === 0) {
+    return true
+  } else if (stacks[endStack].length > 0 && (stacks[startStack].slice(-1) < stacks[endStack].slice(-1))) {
+    return true
+  } else if ((stacks[startStack] > stacks[endStack].slice(-1)) || (stacks[startStack].length = 0) || (stacks[startStack] != a || b || c)) {
+    return false
+  }
 
 }
 
 // What is a win in Towers of Hanoi? When should this function run?
 const checkForWin = () => {
-  // Your code here
+  if ((stacks["b"].length === 4) || (stacks["c"].length === 4)) {
+    return true
+  } else {
+    return false
+  }
 
 }
 
 // When is this function called? What should it do with its argument?
 const towersOfHanoi = (startStack, endStack) => {
-  // Your code here
+  movePiece(startStack, endStack)
+
+  // take in the two args
+  // check if isLegal
+  // if true, movePiece
+  // if false, display error message
+  // call checkForWin
+  // if true, display win message
+  // if false, keep playing 
 
 }
 
